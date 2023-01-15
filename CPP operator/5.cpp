1ce076f80176e138b226b6786d6765435382bd0e
Nazrul7363 <nazrulansari8011@gmail.com>
2023-01-15 22:58:28 +0530

    assignment

--- CPP operator/5.cpp
+++ CPP operator/5.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main()
 {
+    int num, sum=0, rem;
+    cin>>num;
+    while(num!=0)
+    {
+        rem=num%10;
+        sum=sum+rem;
+        num/=10;
+    }
+    cout<<sum;
+
  return 0;
 }
--- CPP operator/6.cpp
+++ CPP operator/6.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 int main()
 {
+    int num1 , num2 ,sum ,pro , dif , div;
+    cin >> num1 >> num2;
+    cout << "sum :"<< num1+num2<<endl<<"Difference :"<<num1-num2<<endl<<"Product :"<<num1*num2<<endl<<"Division :"<<num1/num2<<endl;
  return 0;
 }
