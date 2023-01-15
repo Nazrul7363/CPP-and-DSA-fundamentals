911b4557566b681595d24e2ff515c689e4c7de70
Nazrul7363 <nazrulansari8011@gmail.com>
2023-01-15 13:00:18 +0530

    initial commit

---
+++ First Program Variables and reading IO/2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cin >>ch;
+    cout<< "ASCII Value of "<< ch <<" is : "<<int(ch);
+ return 0;
+}
---
+++ First Program Variables and reading IO/3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int len , br ,area;
+    cout <<"Enter len and br :";
+    cin >>len >> br;
+    area=len *br;
+    cout << "Area of rectangle = "<<area;
+ return 0;
+}
